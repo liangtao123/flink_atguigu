2e0c51b2661397cb3c0401cb66fc20ee5cb5279f
liangtao <liangtaojack@126.com>
2022-11-17 14:45:37 +0800

    Flink使用kafka数据源进行wordcount

--- src/main/java/com/teligen/WordCountDemo.java
+++ src/main/java/com/teligen/WordCountDemo.java
@@ -1,20 +1,29 @@
 package com.teligen;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
+import java.util.Properties;
 
 public class WordCountDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<String> lineDss = env.socketTextStream("localhost", 7777);
+//        DataStreamSource<String> lineDss = env.socketTextStream("localhost", 7777);
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        DataStream<String> lineDss = env.addSource(new FlinkKafkaConsumer<String>(
+                "flink_demo",new SimpleStringSchema(),properties
+        ));
 
         SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOne = lineDss
                 .flatMap((String line, Collector<String> words) -> {
